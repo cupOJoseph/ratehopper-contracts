5dffdc3288de3d6e2d10e03c82293be89dbb0ed4
junta <junta8918@gmail.com>
2024-12-23 11:07:13 +0900

    add testnet network config

--- README.md
+++ README.md
@@ -6,4 +6,10 @@ npx hardhat node --fork https://base.llamarpc.com
 
 // run test againt local node
 yarn test --network localhost --verbose
+
+// deploy to testnet(base sepolia)
+npx hardhat ignition deploy ignition/modules/debtSwap.ts --network baseSepolia
+
+// deploy to mainnet(Base mainnet)
+npx hardhat ignition deploy ignition/modules/debtSwap.ts --network base
 ```
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,32 +1,43 @@
 import { base } from "./node_modules/acorn-walk/dist/walk.d";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+dotenv.config();
 
 const baseUrl = "https://base.llamarpc.com";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.27",
-  networks: {
-    base: {
-      url: baseUrl,
-      // timeout: 1000000,
-      gasPrice: 1700000000,
-      chainId: 8453,
-    },
-    hardhat: {
-      chains: {
-        8453: {
-          hardforkHistory: {
-            london: 1,
-          },
+    solidity: "0.8.27",
+    networks: {
+        base: {
+            url: baseUrl,
+            chainId: 8453,
+            accounts: [process.env.PRIVATE_KEY!],
         },
-      },
-      forking: {
-        url: baseUrl,
-        // blockNumber: 23901547,
-      },
+        baseSepolia: {
+            url: "https://sepolia.base.org",
+            chainId: 84532,
+            accounts: [process.env.PRIVATE_KEY!],
+        },
+        sepolia: {
+            url: "https://eth-sepolia.public.blastapi.io",
+            chainId: 11155111,
+            accounts: [process.env.PRIVATE_KEY!],
+        },
+        hardhat: {
+            chains: {
+                8453: {
+                    hardforkHistory: {
+                        london: 1,
+                    },
+                },
+            },
+            forking: {
+                url: baseUrl,
+                // blockNumber: 23901547,
+            },
+        },
     },
-  },
 };
 
 export default config;
