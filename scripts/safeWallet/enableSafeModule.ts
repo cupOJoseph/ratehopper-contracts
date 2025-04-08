01a82de5f5fa4b985aa8ad1d3eb98856d447f9fc
junta <junta8918@gmail.com>
2025-04-08 16:40:59 +0900

    Update enableSafeModule.ts

--- scripts/safeWallet/enableSafeModule.ts
+++ scripts/safeWallet/enableSafeModule.ts
@@ -3,14 +3,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 async function main() {
+    const ourContractAddress = "0xe551D6Cd14B3b193818513267f41119A04092575";
     const safeWallet = await Safe.init({
         provider: "https://base.llamarpc.com",
         signer: process.env.MY_SAFE_OWNER_KEY!,
         safeAddress: "0x169EeC0c73a76a520e4cFd8Bb982c5237C3f4977",
-        // safeAddress: "0x2f9054Eb6209bb5B94399115117044E4f150B2De",
     });
 
-    const enableModuleTx = await safeWallet.createEnableModuleTx("0xe551D6Cd14B3b193818513267f41119A04092575");
+    const enableModuleTx = await safeWallet.createEnableModuleTx(ourContractAddress);
     const safeTxHash = await safeWallet.executeTransaction(enableModuleTx);
     console.log("Safe enable module transaction");
 
